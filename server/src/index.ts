9f723f073fbce5350de99d435292e9157d69ae97
Hugo Hsi <hhsi@hugodehsign.com>
2024-06-18 05:31:58 -0400

    fixed configuration for dev env and deployment

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,9 @@ import { serveStatic } from 'hono/bun';
 
 const app = new Hono();
 
-app.use('*', serveStatic({ root: '../client/dist' }));
+app.use('*', serveStatic({ root: './client/dist' }));
+app.get('*', serveStatic({ path: './client/dist/index.html' }));
+
+console.log('server is running');
 
 export default app;
