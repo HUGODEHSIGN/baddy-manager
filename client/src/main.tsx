2ffe881d222abbf67e68b100abf05a3ea096cbc5
Hugo Hsi <hhsi@hugodehsign.com>
2024-06-19 09:34:46 -0400

    set up router, added basic routes for testing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,25 @@
+import Providers from '@/providers';
+import { Home, Location, Root } from '@/routes';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/:locationId', element: <Location /> },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Providers>
+      <RouterProvider router={router} />
+    </Providers>
   </React.StrictMode>
 );
---
+++ client/src/routes/index.ts
@@ -0,0 +1,5 @@
+import Home from '@/routes/Home';
+import Location from '@/routes/Location';
+import Root from '@/routes/Root';
+
+export { Home, Location, Root };
