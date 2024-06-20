7486125e7b329dc7532e2fa5b3a2ab635436c3f3
Hugo Hsi <hhsi@hugodehsign.com>
2024-06-20 07:20:10 -0400

    created test route for fetching data, initalized first fetch

--- client/src/routes/fetchTest.tsx
+++ client/src/routes/fetchTest.tsx
@@ -1,9 +1,16 @@
+import { client } from '@/client/eden';
 import { createFileRoute } from '@tanstack/react-router';
 
+async function loader() {
+  return await client.api.locations.index.get();
+}
+
 export const Route = createFileRoute('/fetchTest')({
+  loader,
   component: Page,
 });
 
 function Page() {
-  return <div>Hello /fetchTest!</div>;
+  const { data } = Route.useLoaderData();
+  return <>{data?.map(({ name, id }) => <div key={id}>{name}</div>)}</>;
 }
